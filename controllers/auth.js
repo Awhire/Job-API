d27fbbf02d3ed94101a28acd7a667259114dd2b6
Awhire Elvis <Awhireelvis@gmail.com>
2024-01-20 12:05:47 +0100

    security package

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,24 +1,36 @@
 const User = require('../models/user')
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('../errors')
-const bcrypt = require('bcryptjs')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
 
 // Hashing the users password for security purpose: which mean generating a random bytes and combining it with the password. Hashing is a one way street, meaning it can not be reverse. We store password in a form that protect them. We use libary called bcrypt to hash users.
 
 const register = async (req, res) => {
-    const { name, email, password } = req.body
-
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
-    
-    const tempUser =  { name, email, password: hashedPassword }
-
-    const user = await User.create({...tempUser})
-    res.status(StatusCodes.CREATED).json({ user })
+    const user = await User.create({...req.body})
+    const token = user.createJWT() 
+    res.status(StatusCodes.CREATED).json({ user: {name: user.name}, token })
 }
 
 const login = async (req, res) => {
-    res.send('login user')
+    const {email, password} = req.body
+
+    if(!email || !password){
+        throw new BadRequestError('Please provide email and password')
+    }
+
+    const user = await User.findOne({email})
+    
+    if(!user){
+        throw new UnauthenticatedError('Invalid Email')
+    }
+
+    // compare password
+    const isPasswordCorrect = await user.comparePassword(password)
+    if(!isPasswordCorrect){
+        throw new UnauthenticatedError('Invalid Password')
+    }
+
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({user: {name: user.name}, token})
 }
 
 
