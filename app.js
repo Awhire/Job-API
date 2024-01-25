cb66aa0188fb50aef3f297a9e89c06b07e864e07
Awhire Elvis <Awhireelvis@gmail.com>
2024-01-25 12:19:45 +0100

    swagger docs added

--- app.js
+++ app.js
@@ -9,6 +9,12 @@ const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
 
+// Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
+
 
 const express = require('express')
 const app = express()
@@ -41,10 +47,13 @@ app.use(
 
 
 
+app.get('/', (req, res) => { 
+    res.send('<h1>Jobs API</h1> <a href="/api-docs"> Documentation</a>')
+})
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
+
 // routes
-// app.use('/', (req, res) => { 
-//     res.send('<h1>jobs api</h1> <a href="#"> Swagger Ui </a>')
-//  })
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
